bc83e7cc610db595bb2fa9a8d65b777324f818cc
nadia.co <ngarciaorozco@outlook.com>
2020-06-23 22:53:29 -0600

    Added extra functions
    Needs check on alert

--- Web-Development/projects/Number Guesser/script.js
+++ Web-Development/projects/Number Guesser/script.js
@@ -3,14 +3,27 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+
+//Generate new secret target
 const generateTarget = () => Math.floor(Math.random()*10);
 
+//Validate Human Input
+const checkHumanGuess = (guess) => {
+    if(guess < 0 || theGuess > 9){
+        window.alert("Please enter a number between 0 and 9 (inclusive)");
+    }
+};
+
+//Get Absolute distance between guess and target
+const getAbsoluteDistance = (guess, target) => Math.abs(target - guess);
+
+//Determine who wins according to whose guess is the closest to the target number
+//Returns true if human wins or if there's a tie, flase if computer wins
 function compareGuesses (humanGuess, computerGuess, secretTarget){
-    const humDiff = Math.abs(secretTarget - humanGuess);
-    const compDiff = Math.abs(secretTarget - computerGuess);
-    return humDiff <= compDiff;
+    return getAbsoluteDistance(humanGuess, secretTarget) <= getAbsoluteDistance(computerGuess, secretTarget);
 }
 
+//Increase the winner's score after each round
 function updateScore (winner) {
     if (winner === 'human' ){
         humanScore++;
@@ -19,5 +32,6 @@ function updateScore (winner) {
     }
 }
 
+//Update the round number after each round.
 const advanceRound = () => currentRoundNumber++;
 
