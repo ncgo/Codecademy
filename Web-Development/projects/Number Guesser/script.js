1576717601e37f363538c2bba71cd3d461918006
nadia.co <ngarciaorozco@outlook.com>
2020-06-23 22:17:01 -0600

    Project solution

--- Web-Development/projects/Number Guesser/script.js
+++ Web-Development/projects/Number Guesser/script.js
@@ -3,4 +3,21 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => Math.floor(Math.random()*10);
+
+function compareGuesses (humanGuess, computerGuess, secretTarget){
+    const humDiff = Math.abs(secretTarget - humanGuess);
+    const compDiff = Math.abs(secretTarget - computerGuess);
+    return humDiff <= compDiff;
+}
+
+function updateScore (winner) {
+    if (winner === 'human' ){
+        humanScore++;
+    } else if ( winner === 'computer' ){
+        computerScore++;
+    }
+}
+
+const advanceRound = () => currentRoundNumber++;
 
